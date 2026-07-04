138ea52f50d6f0deedc2384172e210f7e036bcec
Vaishnavidasyam <vaishnavi.41469@gmail.com>
2026-07-05 00:41:20 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // Fallback to localhost if the VITE_API_URL variable isn't defined
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 /* =====================================
