a1be3a895b21e929db033c7bc45989435cb46c86
Vaishnavidasyam <vaishnavi.41469@gmail.com>
2026-07-05 01:06:55 +0530

    Update Quiz.jsx

--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import axios from "axios";
-
+import { API_BASE_URL } from "../utils/constants";
 import { useSearchParams, useNavigate } from "react-router-dom";
 
 import { FiClock, FiCheck, FiX, FiArrowRight, FiAward } from "react-icons/fi";
